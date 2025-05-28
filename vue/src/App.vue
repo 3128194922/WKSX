e45b72dafc590a70b342665abbc7ace063d60891
3128194922 <3128194922@qq.com>
2025-05-28 08:49:18 +0800

    更新

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,17 +1,40 @@
 <script setup>
 import {ref} from 'vue'
-const text =ref("YY")
+const easyhtml=ref("java so easy");
 
-function change()
-{
-  text.value="XX";
+const msg=ref("vue页面");
+
+const htmltext=ref("<span style='color:red'>你好</span>");
+
+const boxstyle=ref("width:100px;height:100px;background-color:green;");
+
+const isok=ref(true);
+
+function change(){
+  easyhtml.value="好好学习，天天向上";
+  msg.value="软工3班实训";
+  boxstyle.value="width:100px;height:100px;background-color:pink;border-radius:50px";
+  isok.value=false;
 }
 </script>
 
 <template>
-  <div>{{text}}</div>
-  <button type="button" @click="change()"></button>
+  <div v-html="easyhtml"></div>
+
+  <p>{{ msg }}</p>
+
+  <div v-text="htmltext"></div>
+
+  <div v-html="htmltext"></div>
+
+  <div v-bind:style="boxstyle"></div>
+
+  <button :disabled="isok">OK按钮</button>
+
+  <button type="button" @click="change()">按钮</button>
+
 </template>
 
 <style scoped>
+
 </style>
